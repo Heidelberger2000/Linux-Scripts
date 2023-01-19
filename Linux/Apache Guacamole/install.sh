0efe66add14ffd7b768c60bf777e71aad9fa8c5e
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-01-19 13:19:13 +0100

    Update install.sh

--- Linux/Apache Guacamole/install.sh
+++ Linux/Apache Guacamole/install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # https://guacamole.apache.org/doc/gug/installing-guacamole.html
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y apache2 git make wget libcairo2-dev libjpeg-turbo8-dev libpng12-dev libtool-bin uuid-dev libavcodec-dev libavformat-dev libavutil-dev libswsccale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libwebsockets-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev
+
+#sudo tar -xzf guacamole-server-1.4.0.tar.gz
+#cd guacamole-server-1.4.0/
+sudo git clone git://github.com/apache/guacamole-server.git
+cd guacamole-server/
+sudo autoreconf -fi
+sudo ./configure --with-init-dir=/etc/init.d
+sudo make
+sudo make install
