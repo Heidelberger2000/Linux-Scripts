24957c018a0bdc6b6634de30fe4d509c753c873c
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-01-19 14:15:10 +0100

    Update install.sh

--- Linux/Piwigo/install.sh
+++ Linux/Piwigo/install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 # https://de.piwigo.org/piwigo-bekommen
+
+wget https://de.piwigo.org/download/dlcounter.php?code=latest
+cp ./dlcounter.php\?code\=latest piwigo.zip
+rm ./dlcounter.php\?code\=latest
+unzip piwigo.zip
+cp -r piwigo/* /var/www/html/*
+sudo chown -R www-data:www-data /var/www/html/
