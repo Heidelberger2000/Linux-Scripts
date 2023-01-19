f4031686874cce42c79dc1d21512922c865e2c13
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-01-19 10:48:28 +0100

    updated 19.01.2023

--- Roxy-Wi/install.sh
+++ Roxy-Wi/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+# Systemaktualisierung
 sudo apt update
 sudo apt upgrade -y
-sudo apt install sudo wget git apache2 -y
+# Benötigte Software wird gezogen 
+sudo apt install sudo wget git apache2 fail2ban python3 python3-pip python3-ldap rsync ansible python3-requests python3-networkx python3-matplotlib python3-bottle python3-future python3-jinja2 python3-peewee python3-distro python3-pymysql python3-psutil python3-paramiko netcat-traditional nmap net-tools lshw dos2unix libapache2-mod-wsgi-py3 openssl sshpass -y
+# Installation beginnt
 cd /var/www/
 sudo git clone https://github.com/hap-wi/roxy-wi.git /var/www/haproxy-wi
-
-sudo apt install apache2 python3 python3-pip python3-ldap rsync ansible python3-requests python3-networkx python3-matplotlib python3-bottle python3-future python3-jinja2 python3-peewee python3-distro python3-pymysql python3-psutil python3-paramiko netcat-traditional nmap net-tools lshw dos2unix libapache2-mod-wsgi-py3 openssl sshpass -y
 sudo chown -R www-data:www-data haproxy-wi/
 sudo cp haproxy-wi/config_other/httpd/roxy-wi_deb.conf /etc/apache2/sites-available/roxy-wi.conf
 sudo a2ensite roxy-wi.conf
 sudo a2enmod cgid ssl proxy_http rewrite
 pip3 install -r haproxy-wi/config_other/requirements_deb.txt
 sudo systemctl restart apache2
-
 pip3 install paramiko-ng
 sudo chmod +x haproxy-wi/app/*.py
 sudo cp haproxy-wi/config_other/logrotate/* /etc/logrotate.d/
@@ -35,26 +35,66 @@ sudo chown -R www-data:www-data /etc/roxy-wi/
 sudo systemctl daemon-reload
 sudo systemctl restart apache2
 sudo systemctl restart rsyslog
-
 cd /var/www/haproxy-wi/app
 sudo ./create_db.py
 sudo chown -R www-data:www-data /var/www/haproxy-wi/
-
 sudo apt install sshpass python3-apt -y
 sudo mkdir /var/www/.ansible
 sudo touch /var/www/.ansible_galaxy
 sudo mkdir /var/www/.ssh
 sudo chown www-data:www-data /var/www/.*
 sudo echo "www-data          ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-
-sudo apt-get install fail2ban -y
-
 sudo cp /var/www/haproxy-wi/config_other/fail2ban/filter.d/* /etc/fail2ban/filter.d/
 sudo cp /var/www/haproxy-wi/config_other/fail2ban/jail.d/* /etc/fail2ban/jail.d/
-
 sudo systemctl start fail2ban
 sudo systemctl enable fail2ban
-
 sudo apt install mariadb-server -y
 sudo apt autoremove -y
-#nano  /etc/roxy-wi/roxy-wi.cfg
+sudo cp /etc/roxy-wi/roxy-wi.cfg /etc/roxy-wi/roxy-wi.cfg.bak
+sudo rm /etc/roxy-wi/roxy-wi.cfg
+
+clear
+echo -e "MySQL Script"
+echo -e ""
+echo -n "Bitte geben Sie die FQDN (example.org) oder IP ein (Std. 127.0.0.1):   "
+read domainip
+echo -n "Bitte geben Sie den Port ein (std. 3306):   "
+read domainport
+echo -n "Bitte geben Sie den mysql_user ein (std. roxy-wi):   "
+read mysqluser
+echo -n "Bitte geben Sie das mysql_password ein (std. roxy-wi):   "
+read mysqlpass
+echo -n "Bitte geben Sie den Dantenbanknamen ein (std. roxy-wi):   "
+read mysqldatabase
+
+sudo mysql -u root <<EOF
+create user '${mysqluser}'@'%';
+create database ${mysqldatabase};
+grant all on ${mysqldatabase}.* to '${mysqluser}'@'%' IDENTIFIED BY '${mysqlpass}';
+grant all on ${mysqldatabase}.* to '${mysqluser}'@'localhost' IDENTIFIED BY '${mysqlpass}';
+EOF
+
+echo "[main]
+# Path to the files destination
+fullpath = /var/www/haproxy-wi
+log_path = /var/log/roxy-wi
+lib_path = /var/lib/roxy-wi
+
+[configs]
+# Folders for configs
+haproxy_save_configs_dir = ${main:lib_path}/configs/hap_config/
+kp_save_configs_dir = ${main:lib_path}/configs/kp_config/
+nginx_save_configs_dir = ${main:lib_path}/configs/nginx_config/
+apache_save_configs_dir = ${main:lib_path}/configs/apache_config/
+
+[mysql]
+enable = 1
+mysql_user = ${mysqluser}
+mysql_password = ${mysqlpass}
+mysql_db = ${mysqldatabase}
+mysql_host = ${domainip}
+mysql_port = ${domainport}" > /etc/roxy-wi/rpxy-wi.cfg
+cd /var/www/haproxy-wi/app
+sudo ./create_db.py
+sudo chown -R www-data:www-data /var/www/haproxy-wi/
+
