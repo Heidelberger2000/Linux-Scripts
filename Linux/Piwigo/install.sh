94e7f0d22784c10a2b46eb3d892674ac380865da
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-01-20 18:54:33 +0100

    Update install.sh

--- Linux/Piwigo/install.sh
+++ Linux/Piwigo/install.sh
@@ -4,7 +4,8 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y install software-properties-common ca-certificates lsb-release apt-transport-https apache2 mariadb-server mariadb-client openssl php8.1 php8.1-mysql php8.1-mbstring php8.1-xml php8.1-curl
+sudo apt install -y software-properties-common ca-certificates lsb-release apt-transport-https apache2 mariadb-server mariadb-client openssl php8.1 php8.1-mysql php8.1-mbstring php8.1-xml php8.1-curl
+sudo apt install -y php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-redis php8.1-intl
 sudo rm /etc/php/8.1/apache2/php.ini
 sudo cp php8.1.ini /etc/php/8.1/apache2/php.ini
 systemctl restart apache2
@@ -18,3 +19,19 @@ rm ./dlcounter.php\?code\=latest
 unzip piwigo.zip
 cp -r piwigo/* /var/www/html/*
 sudo chown -R www-data:www-data /var/www/html/
+clear
+echo -e "MySQL Script"
+echo -e ""
+echo -n "Bitte geben Sie den mysql_username ein:   "
+read mysqluser
+echo -n "Bitte geben Sie das mysql_password ein:   "
+read mysqlpass
+echo -n "Bitte geben Sie den Dantenbanknamen ein:   "
+read mysqldatabase
+
+sudo mysql -u root <<EOF
+create user '${mysqluser}'@'%';
+create database ${mysqldatabase};
+grant all on ${mysqldatabase}.* to '${mysqluser}'@'%' IDENTIFIED BY '${mysqlpass}';
+grant all on ${mysqldatabase}.* to '${mysqluser}'@'localhost' IDENTIFIED BY '${mysqlpass}';
+EOF
