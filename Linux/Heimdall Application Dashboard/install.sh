e01aee51e4f766ff481697bc145e74ddaa74ef2c
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-01-20 16:15:46 +0100

    Update install.sh

--- Linux/Heimdall Application Dashboard/install.sh
+++ Linux/Heimdall Application Dashboard/install.sh
@@ -2,25 +2,25 @@
 
 # https://heimdall.site/
 
-RELEASE=$(curl -sX GET "https://api.github.com/repos/linuxserver/Heimdall/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]'); echo $RELEASE &&\ curl --silent -o Heimdall-${RELEASE}.tar.gz -L "https://github.com/linuxserver/Heimdall/archive/${RELEASE}.tar.gz"
+apt install docker.io docker-compose -y 
+mkdir /heimdall
+cd /heimdall
+mkdir /heimdall-data
+echo "
+version: "2.1"
+services:
+  heimdall:
+    image: linuxserver/heimdall
+    container_name: heimdall
+    volumes:
+      - /heimdall-data:/config
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/London
+    ports:
+      - 80:80
+      - 443:443
+    restart: unless-stopped" > docker-compose.yml
 
-mkdir Heimdall-${RELEASE}
-tar xzvf Heimdall-*.tar.gz -C Heimdall-${RELEASE}
-sudo apt install apache2 php-sqlite3 php-zip
-
-echo "[Unit]
-Description=Heimdall
-After=network.target
-[Service]
-Restart=always
-RestartSec=5
-Type=simple
-User=$USER
-Group=$USER
-WorkingDirectory=/home/lgtm/Heimdall-${RELEASE}
-ExecStart="/usr/bin/php" artisan serve --host 0.0.0.0 --port 7889
-TimeoutStopSec=30
-[Install]
-WantedBy=multi-user.target" > heimdall.service
-sudo cp heimdall.service /etc/systemd/system/heimdall.service
-sudo systemctl enable --now heimdall.service
+docker-compose up -d
