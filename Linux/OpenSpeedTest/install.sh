06185db993482959d4b7557467466b7fbfb11fc7
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-01-20 16:46:53 +0100

    Update install.sh

--- Linux/OpenSpeedTest/install.sh
+++ Linux/OpenSpeedTest/install.sh
@@ -7,5 +7,5 @@ sudo apt upgrade -y
 sudo apt install -y apache2 wget git
 
 wget https://github.com/Heidelberger2000/Scripts/raw/main/Linux/OpenSpeedTest/OpenSpeedTest-Server_2.1.7_amd64.deb
-sudo apt install ./OpenSpeedTest-Server_2.1.7_amd64.deb
+sudo apt install -y ./OpenSpeedTest-Server_2.1.7_amd64.deb
 sudo apt autoremove -y
