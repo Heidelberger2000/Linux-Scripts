140935aa1a92845a6a595eb3ad2c010bdcf7610a
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-03-08 11:40:18 +0100

    Create install.sh

---
+++ Linux/nextcloud/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install apache2 -y
+sudo systemctl enable apache2 && sudo systemctl start apache2
+sudo apt-get install php8.1 php8.1-cli php8.1-common php8.1-imap php8.1-redis php8.1-snmp php8.1-xml php8.1-zip php8.1-mbstring php8.1-curl php8.1-gd php8.1-mysql -y
+sudo apt install mariadb-server -y
+sudo apt install unzip -y
+sudo systemctl start mariadb && sudo systemctl enable mariadb
+sudo mysql <<EOF
+CREATE DATABASE nextcloud;
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost' IDENTIFIED BY 'HzJiKO56Z7U8iuio9dd2W3w4';
+FLUSH PRIVILEGES;
+exit;
+EOF
+
+cd /var/www/html
+wget https://download.nextcloud.com/server/releases/nextcloud-24.0.1.zip && unzip nextcloud-24.0.1.zip
+chown -R www-data:www-data /var/www/html/nextcloud
+
+
+sudo a2ensite nextcloud.conf && sudo a2enmod rewrite && apachectl -t && sudo systemctl restart apache2
