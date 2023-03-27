bdf52a5223b26fd71219264bcefede0edd9d0875
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-03-27 11:54:31 +0200

    Update install_via_apache.sh

--- Linux/Heimdall Application Dashboard/install_via_apache.sh
+++ Linux/Heimdall Application Dashboard/install_via_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+ip=$(ip a | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
 echo "das Skript benoetigt root-Rechte"
 echo "$(whoami)" | [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
