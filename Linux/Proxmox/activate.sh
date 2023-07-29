e5875becc23e51c1d32374de3be1cf3e47b4b363
Heidelberger2000 <70328107+Heidelberger2000@users.noreply.github.com>
2023-07-29 12:19:52 +0200

    Create activate.sh

---
+++ Linux/Proxmox/activate.sh
@@ -0,0 +1,4 @@
+# https://github.com/Jamesits/pve-fake-subscription
+
+wget https://github.com/Jamesits/pve-fake-subscription/releases/download/v0.0.9/pve-fake-subscription_0.0.9+git-1_all.deb
+dpkg -i pve-fake-subscription_*.deb
